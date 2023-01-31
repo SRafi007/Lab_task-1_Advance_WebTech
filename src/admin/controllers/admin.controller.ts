d2fa73f527f78893221c160ce2228958f55a7fb7
SRafi007 <sadmansakib.rafiaiub@gmail.com>
2023-01-31 21:57:09 +0600

    second commit

--- src/admin/controllers/admin.controller.ts
+++ src/admin/controllers/admin.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller,Get,Param,Post} from "@nestjs/common";
+import {Body, Controller,Delete,Get,Param,Post, Put, Query} from "@nestjs/common";
 import { AdminService } from "../services/admin.service";
 import { userDTO } from "src/DTO's/userDTO";
 
@@ -13,12 +13,23 @@ getUsers(){
 getUserById(@Param("id") Id:string){
     return this.adminService.getUserById(Id);
 }
+@Get("/que")
+getQuary(@Query("sort")sortBy:boolean)
+{
+    return sortBy;
+}
 @Post("/addUser")
 addUser(@Body() newUser:userDTO ){
     return this.adminService.insertUser(newUser);
 }
-@Post("/delete")
+@Post("/delete/:id")
 deleteUserById(@Param("id")Id:string){
     return this.adminService.deleteUserById(Id);
+}
+@Put("/update/:id")
+updateUserById(@Param("id")Id:string, @Body("Name")Name:string,@Body("Mail")Mail:string ){
+    const user={Id:Id, Name:Name, Mail:Mail};
+    
+    return this.adminService.updateUserById(user);
 }
 }
--- src/admin/services/admin.service.ts
+++ src/admin/services/admin.service.ts
@@ -3,9 +3,9 @@ import { userDTO } from "src/DTO's/userDTO";
 
 @Injectable()
 export class AdminService {
-    private DammyData=[{Id:"1",Name:"Rafi1",email:"1@gmail.com"},
-                        {Id:"2",Name:"Rafi2",email:"2@gmail.com"},
-                        {Id:"3",Name:"Rafi3",email:"3@gmail.com"}]
+    private DammyData=[{Id:"1",Name:"Rafi1",Mail:"1@gmail.com"},
+                        {Id:"2",Name:"Rafi2",Mail:"2@gmail.com"},
+                        {Id:"3",Name:"Rafi3",Mail:"3@gmail.com"}]
 
 getUsers() {
     return this.DammyData;
@@ -30,6 +30,28 @@ insertUser(user) {
     return this.DammyData;
 }
 deleteUserById(id) {
+    var count=0;
+
+    while(count<this.DammyData.length){
+
+        if(this.DammyData[count].Id===id)
+        {
+            delete this.DammyData[count];
+            return "Deleted";
+        }
+        count++;
+
+    }
     return "Enter a Valid Used Id";
+}
+updateUserById(user:userDTO){
+    const id=user.Id;
+    const index=this.DammyData.findIndex(e=>e.Id===user.Id);
+    if(!index) {
+        return "Enter a valid Id";
+    }
+    this.DammyData[index]=user;
+    return"Updated";
+
 }
 }
